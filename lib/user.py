d7f393d0d2aad27cd63e5ed62e03e026c138e1e9
Bruce <bruce.hillsworth@gmail.com>
2023-05-04 11:44:45 -0600

    Passed tests

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 
+import ipdb
+
+
 class User:
-    pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+# ipdb.set_trace()
